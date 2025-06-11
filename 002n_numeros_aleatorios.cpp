86856d2295b90f46451ff05624e79e06055a87ab
Gabriel Stefano Peñaloza Gutierrez <gpenalozag@unjbg.edu.pe>
2025-06-10 21:07:20 -0500

    Ejercicio 2 - Generador de números aleatorios, versión final

--- 002n_numeros_aleatorios.cpp
+++ 002n_numeros_aleatorios.cpp
@@ -1,12 +1,25 @@
 #include <iostream>
+#include <windows.h>
 #include <cstdlib>
 #include <ctime>
 using namespace std;
 
 int main(){
+    SetConsoleOutputCP(CP_UTF8);
     srand(time(0));
     int cantidad;
     int valorMaximo;
+    
+    cout << "Ingrese la cantidad de números aleatorios que desea imprimir: ";
+    cin >> cantidad;
+    cout << "Ingrese el valor máximo permitido: ";
+    cin >> valorMaximo;
+
+    cout << "\nNúmeros aleatorios generados\n";
+    for(int i = 0; i < cantidad; i++){
+        cout << rand() % valorMaximo + 1 << " ";   
+    }
+    cout << endl;
 
     return 0;
 }
