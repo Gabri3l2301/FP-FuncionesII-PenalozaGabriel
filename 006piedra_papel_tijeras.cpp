4515d8fc9fefda7ab2571f0f324236fde45ba990
Gabriel Stefano Peñaloza Gutierrez <gpenalozag@unjbg.edu.pe>
2025-06-11 23:25:19 -0500

    Ejercicio 6 - Juego de piedra,papel y tijeras, versión final

--- 006piedra_papel_tijeras.cpp
+++ 006piedra_papel_tijeras.cpp
@@ -13,6 +13,7 @@ int main(){
     int jugadaUser;
     int victoriasPC = 0;
     int victoriasUser = 0;
+
     do{
         cout << "Ingrese un número para jugar:" << endl;
         cout << "1. Piedra" << endl;
@@ -20,41 +21,144 @@ int main(){
         cout << "3. Tijeras" << endl;
         cin >> jugadaUser;
 
+        system("cls");
         jugadaPC = rand() % 3 + 1;
         switch (jugadaUser){
         case 1:
+            cout << "\nJugada del Usuario: " << endl;
+            cout << "    _______"          << endl;
+            cout << "---'   ____)"         << endl;
+            cout << "      (_____)"        << endl;
+            cout << "      (_____)"        << endl;
+            cout << "      (____)"         << endl;
+            cout << "---.__(___)"          << endl;                
             if (jugadaPC == 1){
-                cout << "Empate, ninguno anota puntos..." << endl;
+                cout << "Jugada de la PC: " << endl;
+                cout << "    _______"       << endl;
+                cout << "---'   ____)"      << endl;
+                cout << "      (_____)"     << endl;
+                cout << "      (_____)"     << endl;
+                cout << "      (____)"      << endl;
+                cout << "---.__(___)"       << endl;
+                cout << "Empate, ninguno anota puntos...\n" << endl;
+                cout << "Victorias de la PC: " << victoriasPC << endl;
+                cout << "Victorias del usuario: " << victoriasUser << endl << endl;
             }else if (jugadaPC == 2){
-                cout << "Punto para la PC..." << endl;
+                cout << "Jugada de la PC: "     << endl;
+                cout << "     _______"          << endl;
+                cout << "---'    ____)____"     << endl;
+                cout << "           ______)"    << endl;
+                cout << "          _______)"    << endl;
+                cout << "         _______)"     << endl;
+                cout << "---.__________)"       << endl;
+                cout << "Punto para la PC...\n" << endl;
                 victoriasPC++;
+                cout << "Victorias de la PC: " << victoriasPC << endl;
+                cout << "Victorias del usuario: " << victoriasUser << endl << endl;
             }else{
-                cout << "Punto para el usuario..." << endl;
+                cout << "Jugada de la PC: "     << endl;
+                cout << "    _______"           << endl;
+                cout << "---'   ____)____"      << endl;
+                cout << "          ______)"     << endl;
+                cout << "       __________)"    << endl;
+                cout << "      (____)"          << endl;
+                cout << "---.__(___)"           << endl;
+                cout << "Punto para el usuario...\n" << endl;
                 victoriasUser++;
+                cout << "Victorias de la PC: " << victoriasPC << endl;
+                cout << "Victorias del usuario: " << victoriasUser << endl << endl;
             }
             break;
+            
         case 2:
+            cout << "\nJugada del Usuario: " << endl;
+            cout << "     _______"          << endl;
+            cout << "---'    ____)____"     << endl;
+            cout << "           ______)"    << endl;
+            cout << "          _______)"    << endl;
+            cout << "         _______)"     << endl;
+            cout << "---.__________)"       << endl;
             if (jugadaPC == 1){
-                cout << "Punto para el usuario..." << endl;
+                cout << "Jugada de la PC: " << endl;
+                cout << "    _______"       << endl;
+                cout << "---'   ____)"      << endl;
+                cout << "      (_____)"     << endl;
+                cout << "      (_____)"     << endl;
+                cout << "      (____)"      << endl;
+                cout << "---.__(___)"       << endl;
+                cout << "Punto para el usuario...\n" << endl;
                 victoriasUser++;
+                cout << "Victorias de la PC: " << victoriasPC << endl;
+                cout << "Victorias del usuario: " << victoriasUser << endl << endl;
             }else if (jugadaPC == 2){
-                cout << "Empate, ninguno anota puntos..." << endl;
+                cout << "Jugada de la PC: "     << endl;
+                cout << "     _______"          << endl;
+                cout << "---'    ____)____"     << endl;
+                cout << "           ______)"    << endl;
+                cout << "          _______)"    << endl;
+                cout << "         _______)"     << endl;
+                cout << "---.__________)"       << endl;
+                cout << "Empate, ninguno anota puntos...\n" << endl;
+                cout << "Victorias de la PC: " << victoriasPC << endl;
+                cout << "Victorias del usuario: " << victoriasUser << endl << endl;
             }else{
-                cout << "Punto para la PC..." << endl;
+                cout << "Jugada de la PC: "     << endl;
+                cout << "    _______"           << endl;
+                cout << "---'   ____)____"      << endl;
+                cout << "          ______)"     << endl;
+                cout << "       __________)"    << endl;
+                cout << "      (____)"          << endl;
+                cout << "---.__(___)"           << endl;
+                cout << "Punto para la PC...\n" << endl;
                 victoriasPC++;
+                cout << "Victorias de la PC: " << victoriasPC << endl;
+                cout << "Victorias del usuario: " << victoriasUser << endl << endl;
             }
             break;
+
         case 3:
+            cout << "\nJugada del Usuario: " << endl;
+            cout << "    _______"           << endl;
+            cout << "---'   ____)____"      << endl;
+            cout << "          ______)"     << endl;
+            cout << "       __________)"    << endl;
+            cout << "      (____)"          << endl;
+            cout << "---.__(___)"           << endl;
             if (jugadaPC == 1){
-                cout << "Punto para la PC..." << endl;
+                cout << "Jugada de la PC: " << endl;
+                cout << "    _______"       << endl;
+                cout << "---'   ____)"      << endl;
+                cout << "      (_____)"     << endl;
+                cout << "      (_____)"     << endl;
+                cout << "      (____)"      << endl;
+                cout << "---.__(___)"       << endl;
+                cout << "Punto para la PC...\n" << endl;
                 victoriasPC++;
-                
+                cout << "Victorias de la PC: " << victoriasPC << endl;
+                cout << "Victorias del usuario: " << victoriasUser << endl << endl;
             }else if (jugadaPC == 2){
-                cout << "Punto para el usuario..." << endl;
+                cout << "Jugada de la PC: "     << endl;
+                cout << "     _______"          << endl;
+                cout << "---'    ____)____"     << endl;
+                cout << "           ______)"    << endl;
+                cout << "          _______)"    << endl;
+                cout << "         _______)"     << endl;
+                cout << "---.__________)"       << endl;
+                cout << "Punto para el usuario...\n" << endl;
                 victoriasUser++;
+                cout << "Victorias de la PC: " << victoriasPC << endl;
+                cout << "Victorias del usuario: " << victoriasUser << endl << endl;
             }else{
-                
-                cout << "Empate, ninguno anota puntos..." << endl;
+                cout << "Jugada de la PC: "     << endl;
+                cout << "    _______"           << endl;
+                cout << "---'   ____)____"      << endl;
+                cout << "          ______)"     << endl;
+                cout << "       __________)"    << endl;
+                cout << "      (____)"          << endl;
+                cout << "---.__(___)"           << endl;
+                cout << "Empate, ninguno anota puntos...\n" << endl;
+                cout << "Victorias de la PC: " << victoriasPC << endl;
+                cout << "Victorias del usuario: " << victoriasUser << endl << endl;
             }
             break;
         
@@ -64,5 +168,11 @@ int main(){
         }
     }while(victoriasPC < 3 && victoriasUser < 3);
     
+    if (victoriasPC == 3){
+        cout << "La PC ganó, buena suerte para la próxima" << endl;
+    }else{
+        cout << "¡Usted ha ganado, muchas felicitaciones!" << endl;
+    }
+
     return 0;
 }
